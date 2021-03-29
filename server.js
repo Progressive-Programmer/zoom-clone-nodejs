d1ab4286476436f6d968025b6b9c6e502b6cecf0
Aman <jaswal.amansingh@gmail.com>
2021-03-29 17:42:06 +0530

    second commit

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const {v4: uuidv4 } = require('uuid');
 
-
+app.set('view engine', 'ejs');
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.status(200).send('hello world')
+  res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room', (req, res)=> {
+    res.render('room', {roomId: req.params.room})
 })
 
 server.listen(3030);
